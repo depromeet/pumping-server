dd29c7653a62db1ac94033a9318605920baa7356
semi-cloud <sksmsrkdtpal@naver.com>
2023-05-29 23:35:58 +0900

    build: 코틀린 버전 1.7로 다운그레이드

--- pumping/build.gradle.kts
+++ pumping/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.8.21"
-	kotlin("plugin.spring") version "1.8.21"
+	kotlin("jvm") version "1.7.20"
+	kotlin("plugin.spring") version "1.7.20"
 }
 
 group = "com.dpm"
