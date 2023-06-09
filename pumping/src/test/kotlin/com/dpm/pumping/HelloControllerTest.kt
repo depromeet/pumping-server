41f4f76e8ea0219146df2c20b4401c9cd82fc37f
semi-cloud <sksmsrkdtpal@naver.com>
2023-06-09 23:54:42 +0900

    refactor: mockMvc와 RestDocs 자동 구성 환경으로 변경 (#28)

--- pumping/src/test/kotlin/com/dpm/pumping/HelloControllerTest.kt
+++ pumping/src/test/kotlin/com/dpm/pumping/HelloControllerTest.kt
@@ -1,36 +1,22 @@
 package com.dpm.pumping
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
-import org.springframework.restdocs.RestDocumentationContextProvider
-import org.springframework.restdocs.RestDocumentationExtension
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
 import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
 @WebMvcTest(HelloController::class)
-@ExtendWith(RestDocumentationExtension::class)
-class HelloControllerTest {
-
-    private lateinit var mockMvc: MockMvc
-
-    @BeforeEach
-    fun setUp(webApplicationContext: WebApplicationContext?, restDocumentation: RestDocumentationContextProvider?) {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext!!)
-            .apply<DefaultMockMvcBuilder>(documentationConfiguration(restDocumentation))
-            .build()
-    }
+@AutoConfigureRestDocs
+class HelloControllerTest(
+    @Autowired val mockMvc: MockMvc
+){
 
     @Test
     fun test() {
--- pumping/src/test/kotlin/com/dpm/pumping/record/RecordControllerTest.kt
+++
@@ -1,5 +0,0 @@
-package com.dpm.pumping.record
-
-import org.junit.jupiter.api.Assertions.*
-
-internal class RecordControllerTest
