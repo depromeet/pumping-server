2421eebc906e615404740f259274ec13fcdbfb4a
gyucheol <ggyucheol0@gmail.com>
2023-06-18 12:38:26 +0900

    feat: auth controller

--- pumping/src/main/kotlin/com/dpm/pumping/auth/presentation/AuthController.kt
+++ pumping/src/main/kotlin/com/dpm/pumping/auth/presentation/AuthController.kt
@@ -9,10 +9,13 @@ import com.dpm.pumping.auth.oauth2.dtos.OAuth2LoginResponse
 import com.dpm.pumping.auth.oauth2.dtos.SignUpRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+
+
 @RequestMapping("/api/v1")
 @RestController
 class AuthController(
     private val oAuth2AppleClient: OAuth2AppleClient,
+    private val authService: AuthService,
 ) {
 
     @GetMapping("/oauth2/apple")
@@ -20,4 +23,14 @@ class AuthController(
         return ResponseEntity.ok(oAuth2AppleClient.getRedirectUrl())
     }
 
+    @PostMapping("/oauth2/apple/login")
+    fun login(@RequestBody request: AppleLoginRequest): ResponseEntity<OAuth2LoginResponse> {
+        val appleUserId = oAuth2AppleClient.getAppleUserId(request.idToken)
+        return ResponseEntity.ok(authService.login(appleUserId))
+    }
+
+    @PostMapping("/sign-up")
+    fun signUp(@RequestBody request: SignUpRequest): ResponseEntity<AccessTokenResponse> {
+        return ResponseEntity.ok(authService.signUp(request))
+    }
 }
