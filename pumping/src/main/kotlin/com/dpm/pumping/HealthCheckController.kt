70dc704eaf7c4608836896057cd7dfa58f8f4658
gyucheol <ggyucheol0@gmail.com>
2023-06-18 12:33:15 +0900

    feat: health check controller for alb

---
+++ pumping/src/main/kotlin/com/dpm/pumping/HealthCheckController.kt
@@ -0,0 +1,16 @@
+package com.dpm.pumping
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthCheckController {
+
+    @GetMapping
+    fun health(): ResponseEntity<String> {
+        return ResponseEntity.ok("ok")
+    }
+}
