776917837672ca7f563d5d509dd13f02c344e206
gyucheol <ggyucheol0@gmail.com>
2023-06-19 20:25:32 +0900

    test: access token test

---
+++ pumping/src/test/kotlin/com/dpm/pumping/auth/application/JwtTokenProviderTest.kt
@@ -0,0 +1,26 @@
+package com.dpm.pumping.auth.application
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import java.time.Instant
+import java.time.LocalDateTime
+import java.util.*
+
+@ActiveProfiles("test")
+@SpringBootTest
+class JwtTokenProviderTest(
+    @Autowired val jwtTokenProvider: JwtTokenProvider
+) {
+
+    @Test
+    fun `액세스 토큰의 만료시간이 1초이다`() {
+        val now = Date.from(Instant.now())
+
+        val accessToken = jwtTokenProvider.generateAccessToken("asd")
+        assertThat(now.before(accessToken.expiredTime)).isTrue
+    }
+
+}
