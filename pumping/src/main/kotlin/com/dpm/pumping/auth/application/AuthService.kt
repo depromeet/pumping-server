a419398b6c298301396e31098cd6d82ded044ba6
gyucheol <ggyucheol0@gmail.com>
2023-06-21 23:12:19 +0900

    refactor: init login logic

--- pumping/src/main/kotlin/com/dpm/pumping/auth/application/AuthService.kt
+++ pumping/src/main/kotlin/com/dpm/pumping/auth/application/AuthService.kt
@@ -9,6 +9,7 @@ import com.dpm.pumping.user.domain.User
 import com.dpm.pumping.user.domain.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import software.amazon.awssdk.utils.Platform
 
 @Transactional
 @Service
@@ -24,13 +25,7 @@ class AuthService(
                 userRepository.save(User.createWithOAuth(platform))
                 return OAuth2LoginResponse(null, null, platform.loginType, platform.oauth2Id)
             }
-        val token = jwtTokenProvider.generateAccessToken(user.uid)
-        return OAuth2LoginResponse(
-            accessToken = token.accessToken,
-            expiredTime = token.expiredTime,
-            loginType = user.platform.loginType,
-            oauth2Id = user.platform.oauth2Id
-        )
+        return OAuth2LoginResponse(null, null, user.platform.loginType, user.platform.oauth2Id)
     }
 
     fun signUp(request: SignUpRequest): AccessTokenResponse {
