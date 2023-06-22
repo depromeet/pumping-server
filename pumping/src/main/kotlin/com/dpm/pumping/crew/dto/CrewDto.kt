ecc1dadc61e300153569ac160162c90ce6e3005a
Gunho Park <devgun@kakao.com>
2023-06-23 04:17:29 +0900

    feat: get crew by crew id (#48)

---
+++ pumping/src/main/kotlin/com/dpm/pumping/crew/dto/CrewDto.kt
@@ -0,0 +1,14 @@
+package com.dpm.pumping.crew.dto
+
+data class CreateCrewRequest(
+    val crewName: String,
+    val goalCount: Int
+)
+
+data class CrewResponse(
+    val crewId: String?,
+    val crewName: String?,
+    val goalCount: Int?,
+    val code: String?,
+    val participants: List<String>?
+)
