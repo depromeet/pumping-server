85ded48992b023384a43221c2a40025ab0f51a21
giron <ggyucheol0@gmail.com>
2023-07-04 16:07:05 +0900

    Feat/78 exception handler 및 리펙토링(#78) (#79)
    * docs: add user api docs in index.html
    
    * feat: global exception handler
    
    * refactor: delete any and duplicated `@autowired`
    
    * refactor: delete unused string format

---
+++ pumping/src/main/kotlin/com/dpm/pumping/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,27 @@
+package com.dpm.pumping.exception
+
+import io.jsonwebtoken.JwtException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.ErrorResponse
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import java.time.LocalDateTime
+
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    @ExceptionHandler(IllegalArgumentException::class, IllegalStateException::class, JwtException::class)
+    fun businessExceptionHandler(e: Exception?): ResponseEntity<Any>? {
+        val currentRequestAttributes = RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes
+        val requestUrl = currentRequestAttributes?.request!!.requestURI
+        log.info("time:${LocalDateTime.now()},url:${requestUrl},message:${e}")
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e!!.message)
+    }
+}
